487dc15382ebab7b3d4fba4b7e2046849528d1ef
AndrosSinergitec <51095324+AndrosSinergitec@users.noreply.github.com>
2020-11-18 22:01:53 -0600

    espacio en aplicaciones

--- app/src/main/java/com/sienrgitec/todocar/configuracion/Globales.java
+++ app/src/main/java/com/sienrgitec/todocar/configuracion/Globales.java
@@ -13,8 +13,8 @@ import com.sienrgitec.todocar.modelos.opPedidoProveedor;
 import java.util.ArrayList;
 
 public class Globales  {
-    public static String URL          = "http://192.168.1.102:8083/painal/rest/painalService/";
-    //public static  String  URL = "http://sinergitecdemo.ddns.net:8083/painal/rest/painalService/";
+    //public static String URL          = "http://192.168.1.102:8083/painal/rest/painalService/";
+    public static  String  URL = "http://sinergitecdemo.ddns.net:8083/painal/rest/painalService/";
 
     public static Integer vg_iArticulo = 0;
     public static Integer vg_iPartida = 0;
